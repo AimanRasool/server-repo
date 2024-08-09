8c4483beec92d3a1dbeb2725043e01a1262d5cd4
AimanRasool <aimanrasool474@gmail.com>
2024-08-09 15:19:43 +0500

    added client url

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:["https://client-repo.vercel.app/"],
+  methods:"POST",
+  credentials:true
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
